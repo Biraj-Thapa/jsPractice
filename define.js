5352c087fe5f8094823fbdc5b517a30ab78e96b5
Biraj Thapa <birajthapa983@gmail.com>
2024-03-24 21:18:14 +0545

    added defination

---
+++ define.js
@@ -0,0 +1,16 @@
+// js is a dynamically typed/ weakly typed language it means that we should not define any type
+let name="Biraj"; //this is string but we shouldnot define its type
+let age=20;//this is number
+
+// single threaded
+//synchronous language
+//used to add interactivity to the website
+//js is the langauge of web
+//js can be used for both server and client side
+
+//we use camelCase
+let firstName ="Biraj"
+console.log(firstName);
+
+
+ 
