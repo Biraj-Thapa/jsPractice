e7425689bde07ab6ddd90024f37c83c2cb4d3ba3
Biraj Thapa <birajthapa983@gmail.com>
2024-03-28 14:28:41 +0545

    reversse a string

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+function revString(str){
+    let numToStr= str.toString()
+    return(numToStr.split("").reverse().join(""))
+
+}
+console.log(revString("Biraj"))
+console.log(revString(10))
+console.log(revString("Thapa007"))
