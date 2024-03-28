09f4110b0261cfdf4578a7a685662bf8c95df6e0
Biraj Thapa <birajthapa983@gmail.com>
2024-03-28 13:18:17 +0545

    oddEven

---
+++ oddEven.js
@@ -0,0 +1,10 @@
+function checkNum(n){
+    if(n%2==0){
+        return ("number is even")
+    }
+    else{
+        return ("number is odd")
+    }
+}
+console.log(checkNum(5))
+console.log(checkNum(10))
