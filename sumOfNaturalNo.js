0638868a7654b114b4a61a388f829fc6ff0d23de
Biraj Thapa <birajthapa983@gmail.com>
2024-03-28 14:32:37 +0545

    su od natural numbers

---
+++ sumOfNaturalNo.js
@@ -0,0 +1,11 @@
+function sumOfNaturalNumber(n){
+    let sum=0;
+    for(i=1;i<=n;i++){
+        sum=sum+i
+    }
+    return sum;
+}
+console.log(sumOfNaturalNumber(5));
+console.log(sumOfNaturalNumber(7));
+console.log(sumOfNaturalNumber(100));
+
